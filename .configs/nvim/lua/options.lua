d2641d01527a64f03f66527233059df79a7b700e
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-04-14 16:43:08 +0700

    feat: update nvchad config to v2.5

---
+++ .configs/nvim/lua/options.lua
@@ -0,0 +1,14 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+
+-- custom vim options
+o.relativenumber = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.colorcolumn = "80"
