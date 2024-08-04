4c78f913a7b997f9c4df3d8e8dec88ee53237817
husni.zuhdi@accelbyte.net <husninaufalz@gmail.com>
2024-08-04 18:43:10 +0700

    chore: update lspconfig for integrate tailwindcss

--- .configs/nvim/lua/configs/lspconfig.lua
+++ .configs/nvim/lua/configs/lspconfig.lua
@@ -5,8 +5,6 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 local servers = {
-    "html",
-    "cssls",
     "pyright",
     "gopls",
     "terraformls",
@@ -15,6 +13,7 @@ local servers = {
     "docker_compose_language_service",
     "dockerls",
     "marksman",
+    "tsserver",
 }
 
 -- lsps with default config
@@ -26,19 +25,34 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- typescript
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
 -- terraform_lsp
 lspconfig.terraform_lsp.setup{
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
   filetypes = {"hcl"},
+}
+
+-- tailwindcss
+lspconfig.tailwindcss.setup{}
+
+-- html
+-- Enable completion for html and css
+-- Ref: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#html
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+lspconfig.html.setup{
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  filetypes = { "html", "templ", "htmldjango" }
+}
+
+-- cssls
+lspconfig.cssls.setup{
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  filetypes = { "html", "templ", "htmldjango", "css", "scss", "less" }
 }
 
 return {on_attach, capabilities}
