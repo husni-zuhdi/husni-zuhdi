bbf01d81a873175a5de65ed3b0ec0058ffbe679b
Husni Zuhdi <husninaufalz@gmail.com>
2026-07-13 22:17:24 +0700

    chore: enable markdown rendering plugin

--- .configs/nvim/lua/plugins/init.lua
+++ .configs/nvim/lua/plugins/init.lua
@@ -118,17 +118,17 @@ return {
       return M
     end,
   },
-  { "nvim-mini/mini.nvim", version = "*" },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+    ft = { "markdown" },
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
   },
-
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
